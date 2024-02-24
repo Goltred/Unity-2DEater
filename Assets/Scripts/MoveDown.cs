4675693a141519ac88157f041940d0645ae5c8fa
Goltred <goltred@hotmail.com>
2024-02-24 12:24:26 -0500

    Added prefab and MoveDown component

---
+++ Assets/Scripts/MoveDown.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    public float FallSpeed = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-transform.up * FallSpeed * Time.deltaTime);
+    }
+}
